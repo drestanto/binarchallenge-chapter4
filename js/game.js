b46a7e5b43fd47668280d8a58eae25e83f908bf2
drestanto <dyas@live.com>
2021-08-09 15:10:28 +0700

    chooseBatu done

--- js/game.js
+++ js/game.js
@@ -1,12 +1,25 @@
+// Deklarasi DOM
+const resultBox = document.getElementById("result-div");
+const resultName = document.getElementById("result-p");
+const playerBatu = document.getElementById("player-batu");
+const playerKertas = document.getElementById("player-kertas");
+const playerGunting = document.getElementById("player-gunting");
+const comBatu = document.getElementById("com-batu");
+const comKertas = document.getElementById("com-kertas");
+const comGunting = document.getElementById("com-gunting");
+
 // Pilihan COM
 // mengembalikan pilihan computer berupa string "batu", "gunting", atau "kertas"
 function pilihanCom() {
     const pil = Math.floor(Math.random() * 3);
     if (pil==0) {
+        comBatu.style.backgroundColor = "#C4C4C4";
         return "batu";
     } else if (pil==1) {
+        comKertas.style.backgroundColor = "#C4C4C4";
         return "kertas";
     } else { // pil==2
+        comGunting.style.backgroundColor = "#C4C4C4";
         return "gunting";
     }
 }
@@ -54,6 +67,12 @@ function showHasil(hasil) { // procedure
     }
 }
 
+// MAIN
+resultBox.style.visibility = "hidden";
+
+// ----------- ====== onclick function ====== ----------
+
+
 // resultBox, (player/com)(Batu/Gunting/Kertas) adalah variabel global
 // I.S = apapun
 // F.S = ditengah2 tertulsi VS, semua highlight ilang
@@ -67,15 +86,13 @@ function reset() {
     comGunting.style.removeProperty("background-color");
 }
 
-const resultBox = document.getElementById("result-div");
-const resultName = document.getElementById("result-p");
-const playerBatu = document.getElementById("player-batu");
-const playerKertas = document.getElementById("player-kertas");
-const playerGunting = document.getElementById("player-gunting");
-const comBatu = document.getElementById("com-batu");
-const comKertas = document.getElementById("com-kertas");
-const comGunting = document.getElementById("com-gunting");
-
-resultBox.style.visibility = "hidden";
-showHasil("com");
-// reset();
+function chooseBatu() {
+    reset();
+    playerBatu.style.backgroundColor = "#C4C4C4";
+    let player = "batu"
+    console.log("Pilihan Player 1 = " + player);
+    com = pilihanCom();
+    console.log("Pilihan Computer = " + com);
+    result = rockPaperScissors(player, com);
+    showHasil(result);
+}
